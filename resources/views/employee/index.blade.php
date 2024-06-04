44017fda9dedc741d5834894bd524f72e01799c8
Justine-01 <enriquezjustine18@gmail.com>
2024-06-04 11:38:40 +0800

    comment

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,64 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
- 
-    <div class="container">
-    <div class="row">
-<div class="col-12 col-sm-6 col-md-3">
-<div class="info-box">
-<span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">CPU Traffic</span>
-<span class="info-box-number">
-10
-<small>%</small>
-</span>
-</div>
-
-</div>
-
-</div>
-
-<div class="col-12 col-sm-6 col-md-3">
-<div class="info-box mb-3">
-<span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">Likes</span>
-<span class="info-box-number">41,410</span>
-</div>
-
-</div>
-
-</div>
-
-
-<div class="clearfix hidden-md-up"></div>
-<div class="col-12 col-sm-6 col-md-3">
-<div class="info-box mb-3">
-<span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">Sales</span>
-<span class="info-box-number">760</span>
-</div>
-
-</div>
-
-</div>
-
-<div class="col-12 col-sm-6 col-md-3">
-<div class="info-box mb-3">
-<span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
-<div class="info-box-content">
-<span class="info-box-text">New Members</span>
-<span class="info-box-number">2,000</span>
-</div>
-
-</div>
-
-</div>
-
-</div>
-
         @if (session('status'))
             <div class="alert alert-success">{{session('status')}}</div>
         @endif
@@ -107,7 +49,7 @@
 
                     <div class="row">
                         <div class="col-md-12 mt-3">
-                            <input type="submit" class="btn btn-danger bg-green" value="Register">
+                            <input type="submit" class="btn btn-danger bg-info" value="Register">
                         </div>
 
                     </div>
@@ -138,9 +80,9 @@
                                     <td>{{$item->phone}}</td>
                                     <td>{{$item->dob}}</td>
                                     <td>
-                                        <a href="{{ url('employee/'.$item->id.'/edit') }}" class="btn btn-info  mx-2 bg-blue">Edit</a>
+                                        <a href="{{ url('employee/'.$item->id.'/edit') }}" class="btn btn-info bg-green">Edit</a>
                                         
-                                        <a href="{{ url('employee/'.$item->id.'/edit') }}" class="btn btn-info mx-1 bg-pink">Delete</a>
+                                        <a href="{{ url('employee/'.$item->id.'/edit') }}" class="btn btn-info bg-red">Delete</a>
                                     </td>
                                 </tr>
                             @endforeach
